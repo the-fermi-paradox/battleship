657f46c6fc5b010afaf42782b32c99f12f7c9d3b
JG <joshgundy55@gmail.com>
2021-07-16 13:54:24 -0500

    Gameboard now passes test suite

--- src/model/Gameboard.js
+++ src/model/Gameboard.js
@@ -3,14 +3,23 @@ import _ from 'lodash';
 class Gameboard {
   constructor() {
     this.coordinates = Array(10).fill(0)
-      .map(() => Array(10).fill({ status: 'empty', ship: {} }));
+      .map(() => Array(10).fill({ status: 'empty', ship: null }));
     this.ships = [];
   }
 
-  placeShip(ship) {
+  placeShip(ship, [x, y]) {
     const clone = _.cloneDeep(this);
     const cloneShip = _.cloneDeep(ship);
     clone.ships.push(cloneShip);
+    clone.coordinates[x] = clone.coordinates[x].map((ele, index) => {
+      if (index >= y && index < y + ship.length) {
+        return {
+          status: 'filled',
+          ship: cloneShip,
+        };
+      }
+      return ele;
+    });
     return clone;
   }
 
@@ -20,7 +29,7 @@ class Gameboard {
     const clone = _.cloneDeep(this);
     clone.coordinates[x][y].status = result;
     const ship = clone.coordinates[x][y]?.ship;
-    clone.coordinates[x][y].ship = ship?.hit(ship, [x, y]) || null;
+    clone.coordinates[x][y].ship = ship?.hit?.(ship, [x, y]) || null;
     return clone;
   }
 }
